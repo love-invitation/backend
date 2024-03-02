7ad1b6748c8273be23d4b9f1d62a68ef111e3734
hong jun ki <ghdwnsrl1991@gmail.com>
2024-03-02 15:50:26 +0900

    [FEAT] 청첩장 단일 조회 기능 구현

--- src/main/java/jun/invitation/domain/product/invitation/dao/InvitationRepository.java
+++ src/main/java/jun/invitation/domain/product/invitation/dao/InvitationRepository.java
@@ -4,5 +4,4 @@ import jun.invitation.domain.product.invitation.domain.Invitation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvitationRepository extends JpaRepository<Invitation, Long> {
-
 }
---
+++ src/main/java/jun/invitation/global/SecurityUtils.java
@@ -0,0 +1,13 @@
+package jun.invitation.global;
+
+import jun.invitation.domain.auth.PrincipalDetails;
+import jun.invitation.domain.user.domain.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+public class SecurityUtils {
+    public static User getCurrentUser() {
+        PrincipalDetails principalDetails = (PrincipalDetails) SecurityContextHolder.
+                getContext().getAuthentication().getPrincipal();
+
+        return principalDetails.getUser();
+    }
+}
