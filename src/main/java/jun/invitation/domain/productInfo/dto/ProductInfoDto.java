f48a069c41934b5aec50011486dfa89649aa980e
hong jun ki <ghdwnsrl1991@gmail.com>
2024-05-13 18:26:33 +0900

    [Modify] ProductInfo: ProductInfoDto field 명 변경 name -> templateName

--- src/main/java/jun/invitation/domain/productInfo/dto/ProductInfoDto.java
+++ src/main/java/jun/invitation/domain/productInfo/dto/ProductInfoDto.java
@@ -1,7 +1,6 @@
 package jun.invitation.domain.productInfo.dto;
 
 import jun.invitation.domain.productInfo.domain.ProductInfo;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +13,7 @@ import static lombok.AccessLevel.*;
 @NoArgsConstructor(access = PROTECTED)
 public class ProductInfoDto {
     private String imageUrl;
-    private String name;
+    private String templateName;
     private BigDecimal price;
 
     private Boolean best;
@@ -22,7 +21,7 @@ public class ProductInfoDto {
 
     public ProductInfoDto(ProductInfo productInfo) {
         this.imageUrl = productInfo.getImageUrl();
-        this.name = productInfo.getName();
+        this.templateName = productInfo.getName();
         this.price = productInfo.getPrice();
         this.best = productInfo.getBest();
         this.newest = productInfo.getNewest();
