b8cfb33c74f6974fe7bb4db53923f13304e17499
hong jun ki <ghdwnsrl1991@gmail.com>
2024-06-18 20:29:09 +0900

    [refactor] annotation 추가 : @AllArgsConstructor, @Data

--- src/main/java/jun/invitation/domain/priority/dto/PriorityDto.java
+++ src/main/java/jun/invitation/domain/priority/dto/PriorityDto.java
@@ -1,14 +1,11 @@
 package jun.invitation.domain.priority.dto;
 
 import jun.invitation.domain.priority.domain.Priority;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PriorityDto {
     private String name;
     private Integer priority;
