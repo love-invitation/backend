e4043557a3382fc846c7523971a7cb80be2ff192
hong jun ki <ghdwnsrl1991@gmail.com>
2024-08-25 22:05:45 +0900

    [refactor] guestbook response에 id값 추가

--- src/main/java/jun/invitation/domain/guestbook/dto/GuestbookResponseDto.java
+++ src/main/java/jun/invitation/domain/guestbook/dto/GuestbookResponseDto.java
@@ -7,10 +7,12 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class GuestbookResponseDto {
+    private Long id;
     private String name;
     private String message;
 
     public GuestbookResponseDto(Guestbook guestbook) {
+        this.id = guestbook.getId();
         this.name = guestbook.getName();
         this.message = guestbook.getMessage();
     }
