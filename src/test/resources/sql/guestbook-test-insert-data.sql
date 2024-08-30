7cb6c46b548ae06a889bfaa0f8eeae3317b59ae9
hong jun ki <ghdwnsrl1991@gmail.com>
2024-08-30 22:28:51 +0900

    [refactor] init data 수정

--- src/test/resources/sql/guestbook-test-insert-data.sql
+++ src/test/resources/sql/guestbook-test-insert-data.sql
@@ -1,3 +1,3 @@
 insert into `guestbook` (`product_id`,`message`,`name`,`password`,`guestbook_id`) values (1,'잘 살아','hong','hong',1);
-insert into `guestbook` (`product_id`,`message`,`name`,`password`,`guestbook_id`) values (1,'건강해','lee','lee',2);
+insert into `guestbook` (`product_id`,`message`,`name`,`password`,`guestbook_id`) values (1,'건강해','lee','test-pw',2);
 insert into `guestbook` (`product_id`,`message`,`name`,`password`,`guestbook_id`) values (1,'싸우지마','park','park',3);
--- src/test/resources/sql/priority-test-insert-data.sql
+++ src/test/resources/sql/priority-test-insert-data.sql
@@ -1,8 +1,8 @@
-insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'article',3,1);
-insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'booking',2,2);
-insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'place',1,3);
-insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'contact',4,4);
-insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'transport',5,5);
-insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'account',7,6);
-insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'gallery',8,7);
+insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'ARTICLE',3,1);
+insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'BOOKING',2,2);
+insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'PLACE',1,3);
+insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'CONTACT',4,4);
+insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'TRANSPORT',5,5);
+insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'ACCOUNT',7,6);
+insert into `priority` (`product_id`,`name`,`priority`,`priority_id`) values (1,'GALLERY',8,7);
 
